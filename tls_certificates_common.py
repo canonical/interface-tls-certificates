6a55b5e27a3c1c73f1b91afdbb1b7e32b95730e3
Liam Young <liam.young@canonical.com>
2020-03-23 11:37:44 +0000

    Add support for application certs
    The idea of an application cert is that it is identical on all
    units. To generate one the interface collects all the published
    sans and cns from the clients and generates one certificate
    which is valid for all of them.

--- tls_certificates_common.py
+++ tls_certificates_common.py
@@ -17,12 +17,36 @@ class CertificateRequest(dict):
                          self.unit_name,
                          self.common_name))
 
+    def resolve_unit_name(self, unit):
+        """Return name of unit associated with this request.
+
+        unit_name should be provided in the relation data to ensure
+        compatability with cross-model relations. If the unit name
+        is absent then fall back to unit_name attribute of the
+        unit associated with this request.
+
+        :param unit: Unit to extract name from
+        :type unit: charms.reactive.endpoints.RelatedUnit
+        :returns: Name of unit
+        :rtype: str
+        """
+        unit_name = unit.received_raw['unit_name']
+        if not unit_name:
+            unit_name = unit.unit_name
+        return unit_name
+
     @property
     def unit_name(self):
-        unit_name = self._unit.received_raw['unit_name']
-        if not unit_name:
-            unit_name = self._unit.unit_name.replace('/', '_')
-        return unit_name
+        return self.resolve_unit_name(unit=self._unit).replace('/', '_')
+
+    @property
+    def application_name(self):
+        """Name of the application which the request came from.
+
+        :returns: Name of application
+        :rtype: str
+        """
+        return self.resolve_unit_name(unit=self._unit).split('/')[0]
 
     @property
     def cert_type(self):
@@ -114,6 +138,86 @@ class CertificateRequest(dict):
         if not rel.endpoint.new_requests:
             clear_flag(rel.endpoint.expand_name('{endpoint_name}.'
                                                 'certs.requested'))
+        data_changed(self._key, sorted(set(self.sans or [])))
+
+
+class ApplicationCertificateRequest(CertificateRequest):
+    """
+    A request for an application consistent certificate.
+
+    This is a request for a certificate that works for all units of an
+    application. All sans and cns are added together to produce one
+    certificate and the same certificate and key are sent to all the
+    units of an application. Only one ApplicationCertificateRequest
+    is needed per application.
+    """
+
+    @property
+    def _key(self):
+        return '.'.join((self._unit.relation.relation_id,
+                         self.common_name))
+
+    @property
+    def sans(self):
+        """Generate a list of all sans from all units of application
+
+        Examine all units of the application and compile a list of
+        all sans. CNs are treated as addition san entries.
+
+        :returns: List of sans
+        :rtype: List[str]
+        """
+        _sans = []
+        for unit in self._unit.relation.units:
+            reqs = unit.received['application_cert_requests'] or {}
+            for cn, req in reqs.items():
+                _sans.append(cn)
+                _sans.extend(req['sans'])
+        return list(set(_sans))
+
+    @property
+    def _request_key(self):
+        return 'application_cert_requests'
+
+    def derive_publish_key(self, unit=None):
+        """Derive the application cert publish key for a unit.
+
+        :param unit: Unit to extract name from
+        :type unit: charms.reactive.endpoints.RelatedUnit
+        :returns: publish key
+        :rtype: str
+        """
+        if not unit:
+            unit = self._unit
+        unit_name = self.resolve_unit_name(unit).replace('/', '_')
+        return '{}.processed_application_requests'.format(unit_name)
+
+    @property
+    def _publish_key(self):
+        return self.resolve_unit_name(unit=self._unit).replace('/', '_')
+
+    def set_cert(self, cert, key):
+        """Send the cert and key to all units of the application
+
+        :param cert: TLS Certificate
+        :type cert: str
+        :param key: TLS Private Key
+        :type cert: str
+        """
+        rel = self._unit.relation
+        for unit in self._unit.relation.units:
+            pub_key = self.derive_publish_key(unit=unit)
+            data = rel.to_publish.get(
+                pub_key,
+                {})
+            data['app_data'] = {
+                'cert': cert,
+                'key': key,
+            }
+            rel.to_publish[pub_key] = data
+        if not rel.endpoint.new_application_requests:
+            clear_flag(rel.endpoint.expand_name(
+                '{endpoint_name}.application.certs.requested'))
         data_changed(self._key, sorted(set(self.sans or [])))
 
 
