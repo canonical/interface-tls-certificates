52d8d17b3610220827a9063a09910cf9ccff8cec
George Kraft <george.kraft@canonical.com>
2023-10-19 12:28:16 -0500

    Update chain field definition for clarity
    Co-authored-by: Adam Dyess <adam.dyess@canonical.com>

--- ops/ops/interface_tls_certificates/model.py
+++ ops/ops/interface_tls_certificates/model.py
@@ -16,6 +16,6 @@ class Data(BaseModel, extra=Extra.allow):
     """Databag from the relation."""
 
     ca: StrictStr = Field(alias="ca")
-    chain: Optional[StrictStr] = Field(None, alias="chain")
+    chain: Optional[StrictStr] = Field(default=None, alias="chain")
     client_cert: StrictStr = Field(alias="client.cert")
     client_key: StrictStr = Field(alias="client.key")
